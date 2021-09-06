f81136d7bcbf0de139bc1036551a4043790e5e28
PuriniMounika <mounupurini2000@gmail.com>
2021-09-06 20:48:42 +0530

    first commit

---
+++ src/arrays/ArrayRange.java
@@ -0,0 +1,31 @@
+package arrays;
+import java.util.Scanner;
+
+public class ArrayRange {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter an length of the array");
+	int n=sc.nextInt();
+	int[]arr=new int[n];
+	System.out.println("Enter the elements of an array");
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=sc.nextInt();
+	}
+	int max=arr[0];
+	int min=arr[0];
+	for(int i=0;i<n;i++)
+	{
+		if(min>arr[i])
+		{
+			min=arr[i];
+		}
+	}
+	int range=max=min;
+	System.out.println(range);
+	
+
+	}
+
+}
